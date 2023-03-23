491a292cb20811d7080e4e6a06d8dab884e87a5a
Serhii Priadkin <sergio.pryadkin@gmail.com>
2023-03-23 20:13:37 +0200

    Learned useNavigate method

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Product() {
+  const navigate = useNavigate();
+
   return (
-    <div>Product</div>
-  )
+    <div>
+      Product
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product{" "}
+      </button>
+      <Link to={`/product/details/5`}>
+        <button>Navigate to Product Detail - 5</button>
+      </Link>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
